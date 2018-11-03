21021436827f84c682a6c252a4219472fdfbfb1f
JoseLvz <oljoselo@hotmail.com>
2018-11-03 12:47:55 -0500

    agregue mi parte

---
+++ Assets/Scripts/Controlador.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controlador : MonoBehaviour {
+
+    public GameObject location;
+    public GameObject Item;
+    public bool Quest1,Quest2;
+
+    int Monsters = 5;
+    public bool Reached;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Quest1 == true)
+        {
+            Debug.Log("Quest accepted");
+        }
+		
+	}
+    void OnTriggerEnter(Collider other)
+    {   
+        if(other.name == "Player")
+        {
+            switch (Monsters)
+            {
+                case 1:
+                    Debug.Log("4left");
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+
+                    break;
+                case 5:
+                    Quest1 = false;
+                    Debug.Log("Quest completed");
+                    Quest2 = true;
+                    break;
+            }
+        }
+        
+    }
+
+}
---
+++ Assets/Scripts/dialogos.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dialogos : MonoBehaviour {
+
+    public string d1;
+    public string d2;
+    public string d3;
+
+    private void Start()
+    {
+        d1 = "mata 5 monstruos por mi";
+        d2 = "ve a las afueras de la ciudad";
+        d3 = "recoge el jarrón del milenio";
+    }
+
+
+}
