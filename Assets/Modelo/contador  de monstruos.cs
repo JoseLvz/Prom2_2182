51b5746ca2ecc0b1ceb567131de9314d3a2260d2
j01000100 <jdpr97@gmail.com>
2018-11-03 11:39:41 -0500

    subi la parte modelo del quest

---
+++ Assets/Modelo/contador  de monstruos.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class contadordemonstruos : MonoBehaviour {
+
+    public int monstruosasesinados;
+    public int monstruospormatar;
+
+}
---
+++ Assets/Modelo/misiones.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class misiones : MonoBehaviour {
+
+    public bool empezarmision;
+    public bool misionterminada;
+    public GameObject afuerasdelaciudad;
+    public GameObject jarrondelmilenio;
+
+}
